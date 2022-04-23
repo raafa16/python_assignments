5abfe41e5e7eea7566aa5e896ad636583638e479
raafa16 <raafa166@gmail.com>
2022-04-23 12:24:02 -0400

    blackbelt option

---
+++ cryptography/blackbelt.py
@@ -0,0 +1,93 @@
+import random
+
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+space = "5"
+
+
+def main():
+    keepGoing = True
+    key = "XPMGTDHLYONZBWEARKJUFSCIQV"
+
+    while keepGoing:
+        response = menu()
+        if response == "1":
+            plain = input("text to be encoded: ")
+            print(encode(plain, key))
+        elif response == "2":
+            coded = input("code to be decyphered: ")
+            print(decode(coded, key))
+        elif response == "3":
+            key = generate_key()
+            print("New Key: ", key)
+        elif response == "0":
+            print("Thanks for doing secret spy stuff with me.")
+            keepGoing = False
+        else:
+            print("I don't know what you want to do...")
+
+
+def menu():
+    menuTitle = "SECRET DECODER MENU"
+    menuOptions = {
+        0: 'Quit',
+        1: 'Encode',
+        2: 'Decode',
+        3: 'Generate Random Key'
+    }
+
+    # Show the menu
+    print(menuTitle)
+    for key in menuOptions.keys():
+        print(key, ')', menuOptions[key])
+
+    response = input("What is your choice?: ")
+
+    return response
+
+
+def encode(plain, key):
+    encodedString = ""
+
+    for char in plain:
+        # convert from lowercase to uppercase
+        char = char.upper()
+        # if the char is a letter present in alpha
+        if char in alpha:
+            char = char
+            # find the index of the letter in alpha
+            idx = alpha.index(char)
+
+            # append key to the encoded string
+            encodedString += key[idx]
+        # if the char is a space, replace with 5
+        elif char == ' ':
+            encodedString += space
+
+    return encodedString
+
+
+def decode(coded, key):
+    decodedString = ""
+
+    for char in coded:
+        if char in key:
+            # find the index of the char in key
+            idx = key.index(char)
+
+            # append letter to the decoded string
+            decodedString += alpha[idx]
+        # if char is a 5, replace with a space
+        elif char == '5':
+            decodedString += ' '
+
+    return decodedString
+
+
+def generate_key():
+    # use the letters to generate random key
+    return ''.join(random.sample(alpha, len(alpha)))
+
+
+if __name__ == "__main__":
+
+    main()
